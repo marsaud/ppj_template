4bc421397285dc04777fa52b54ab24044858093b
Fabrice MARSAUD <marsaud.fabrice@neuf.fr>
2013-03-18 18:21:46 +0100

    in progress

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $variant = $this->params->get('templatevariant');
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
+        <jdoc:include type="head" />
         <title><?php echo $this->title; ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <link rel="stylesheet" href="http://cafepoisplumes.fr/templates/ppj_template/css/poisplumes_<?php echo $variant; ?>.css" type="text/css" media="screen" />
@@ -16,7 +17,8 @@ $variant = $this->params->get('templatevariant');
     <body>
         <div class="main-layout">
             <div class="banner">
-                <h1>CAFÉ POIS PLUMES</h1>
+                <!--h1>CAFÉ POIS PLUMES</h1-->
+                <img src="http://cafepoisplumes.fr/templates/ppj_template/images/poisplumes/logo_moyen.png" />
             </div>
             <div>
                 <jdoc:include type="modules" name="menu-position" />
